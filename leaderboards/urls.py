a865d461abbaca500741712a9d917b20d156791f
Sean Kearns <seanpjk@gmail.com>
2024-02-25 03:39:31 +0000

    Did some removing.

--- leaderboards/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import leaderboard_view, submit_score
-
-app_name = 'leaderboards'
-urlpatterns = [
-    path('', leaderboard_view, name='leaderboard'),
-    path('submit_score/', submit_score, name='submit')
-]
